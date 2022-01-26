57d4c267de3cdd210016a116874ecd0e29ae1006
Krister Borge <kristebo@ifi.uio.no>
2022-01-26 12:23:07 +0100

    add stuff

--- src/app.py
+++ src/app.py
@@ -1,68 +1,60 @@
 import json
-import flask
-import requests_oauthlib
-import os
+
 import requests
-
-CLIENT_ID = os.environ.get("CLIENT_ID")
-CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-
-AUTHORIZATION_BASE_URL = os.environ.get("AUTH_URL")
-TOKEN_URL = os.environ.get("TOKEN_URL")
-API_BASE = os.environ.get("API_BASE_URL")
-
-# This allows us to use a plain HTTP callback
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-app = flask.Flask(__name__)
+from flask import Flask, redirect, url_for, render_template, request
+import os
+import dotenv
+from src.unicorn_api.OAuth import OAuthSignIn
 
 
-@app.route("/")
+
+app = Flask(__name__)
+dotenv.load_dotenv()
+client_secret = os.environ.get('CLIENT_SECRET')
+client_id = os.environ.get('CLIENT_ID')
+auth_url= os.environ.get('AUTH_URL')
+print(auth_url)
+token_url=os.environ.get("TOKEN_URL")
+with app.app_context():
+    oauth = OAuthSignIn(client_id, client_secret, auth_url, token_url)
+
+@app.route('/')
 def index():
-    return """
-    <a href="/login">Login with SimpleLogin</a>
-    """
+
+    return render_template('index.html')
 
 
-@app.route("/login")
-def login():
-    simplelogin = requests_oauthlib.OAuth2Session(
-        CLIENT_ID, redirect_uri="http://localhost:5000/callback"
-    )
-    authorization_url, _ = simplelogin.authorization_url(AUTHORIZATION_BASE_URL)
-    print(authorization_url)
-    return flask.redirect(authorization_url)
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
 
 
-@app.route("/callback")
-def callback():
-    authorization_code=(flask.request.args.get("code"))
-    data = {'grant_type': 'authorization_code', 'code': authorization_code, 'redirect_uri': "http://localhost:5000/callback"}
-    print("requesting access token")
-    access_token_response = requests.post(TOKEN_URL, data=data, verify=False, allow_redirects=True,
-                                          auth=(CLIENT_ID, CLIENT_SECRET))
-
-    print(access_token_response)
-    print("response")
-    print(access_token_response.headers)
-    print('body: ' + access_token_response.text)
-
-    # we can now use the access_token as much as we want to access protected resources.
-    tokens = json.loads(access_token_response.text)
-    access_token = tokens['access_token']
-    print("access token: " + access_token)
-
-    api_call_headers = {'Authorization': 'Bearer ' + access_token}
-    api_call_response = requests.get(API_BASE+"api/competitions/", headers=api_call_headers, verify=False)
-    return f"""
-    {data}</br>
-    {access_token_response.request.url}</br>
-    {access_token_response.headers.values()}</br>
-    {access_token}</br>
-    <div>{api_call_response.text}</div>
-    """
+@app.route('/login/')
+def oauth_authorize():
+    authorizeurl=oauth.authorize()
+    print(authorizeurl)
+    return redirect(authorizeurl)
 
 
+@app.route('/callback/')
+def oauth_callback():
+    global access
+    access = oauth.callback()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("return.html",access_token=access)
+@app.route('/testapi/')
+def testapi():
+    api=request.args.get('apiurl')
+    api_call_headers = {'Authorization': 'Bearer ' + access}
+    api_call_response = requests.get(api, headers=api_call_headers,
+                                     verify=False)
+
+    jsondata=json.loads(api_call_response.text)
+
+
+    return render_template('testapi.html', compodata=jsondata)
+
+
+if __name__ == '__main__':
+
+    app.run()
---
+++ src/unicorn_api/OAuth.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+import flask
+import requests
+import requests_oauthlib
+
+from flask import current_app
+
+class OAuthSignIn(object):
+    providers = None
+
+    def __init__(self, client_id, client_secret,auth_url,token_url):
+        print(auth_url)
+        self.auth_url=auth_url
+        self.token_url=token_url
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def authorize(self):
+        simplelogin = requests_oauthlib.OAuth2Session(
+            self.client_id, redirect_uri="http://localhost:5000/callback"
+        )
+        print(self.auth_url)
+        authorization_url,_ = simplelogin.authorization_url(self.auth_url)
+        print(authorization_url)
+        return authorization_url
+
+    def callback(self):
+        authorization_code = (flask.request.args.get("code"))
+        data = {'grant_type': 'authorization_code', 'code': authorization_code,
+                'redirect_uri': "http://localhost:5000/callback"}
+        print("requesting access token")
+        access_token_response = requests.post(self.token_url, data=data, verify=False, allow_redirects=True,
+                                              auth=(self.client_id, self.client_secret))
+
+        print(access_token_response)
+        print("response")
+        print(access_token_response.headers)
+        print('body: ' + access_token_response.text)
+
+        # we can now use the access_token as much as we want to access protected resources.
+        tokens = json.loads(access_token_response.text)
+        access_token = tokens['access_token']
+        print("access token: " + access_token)
+        return access_token
+
+
